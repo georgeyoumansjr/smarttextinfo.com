3bd3150b123bc6abe8d27adb19132ec49a2cfb2d
georgeyoumansjr <georgeyoumansjr@gmail.com>
2023-01-29 07:14:22 -0800

    implemented frontend and displayed tweets and users on frontend for a username and removed unsued code

--- TwitterDjango/urls.py
+++ TwitterDjango/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import Index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('api.urls')),
     path('', include('api.urls')),
-    # path('', Index),
 ]
