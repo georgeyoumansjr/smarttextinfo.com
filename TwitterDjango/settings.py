d31ccb6a364c3cafd787ed4fa6ba2f191b0d253a
coboaccess <coboaccess@gmail.com>
2023-03-18 10:09:36 -0700

    Prod Changes

--- TwitterDjango/settings.py
+++ TwitterDjango/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = 'django-insecure-61vx9m-4+d^(1=cv28fb3nmwvfji!smqrfg0$8xrmpdlkd1(==
 DEBUG = True
 
 ALLOWED_HOSTS = [
+'www.smarttextinfo.com',
 'smarttextinfo.com',
 '127.0.0.1',
 'localhost',
@@ -60,6 +61,7 @@ MIDDLEWARE  = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_auth.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'TwitterDjango.urls'
@@ -125,18 +127,6 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth', # not necessary right now but maybe in the future
     'social_core.backends.twitter.TwitterOAuth', # same as above
     'django.contrib.auth.backends.ModelBackend', # normal username and password login
-)
-
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
 )
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('GOOGLE_KEY')
