4006d7efa6e4494f30cbc187911d40277bb0d947
coboaccess <coboaccess@gmail.com>
2023-03-24 05:17:13 -0700

    button is disabled unless all the inputs have value

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -3,3 +3,17 @@ $('#submit-button').on('click', function (){
     $('#loading').show();
     $( "#info-text" ).delay(3000).show("slow","swing");
 });
+$(document).ready(function() {
+    // Check input field values on page load
+    checkInputValues();
+    
+    // Check input field values on input change
+    $('.form-control').on('input', checkInputValues);
+  });
+  
+  function checkInputValues() {
+    var allValuesEntered = $('.form-control').filter(function() {
+        return this.value !== '';
+      }).length === $('.form-control').length;
+    $('#submit-button').prop('disabled', !allValuesEntered);
+  }
